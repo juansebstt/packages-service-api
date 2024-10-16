804e92f5a21b68b5421a1d9ec16ac8610004671e
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-15 19:48:12 -0500

    first commit

---
+++ src/main/java/com/packagesserviceapi/PackagesServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.packagesserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagesServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackagesServiceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/packagesserviceapi/PackagesServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packagesserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagesServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
