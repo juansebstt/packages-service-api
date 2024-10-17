09f9661b8d9fbb7e448f0396c4e876404dc99908
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-16 19:33:18 -0500

    Implemented ApiPathConstants

---
+++ src/main/java/com/packagesserviceapi/commons/constants/ApiPathConstants.java
@@ -0,0 +1,8 @@
+package com.packagesserviceapi.commons.constants;
+
+public class ApiPathConstants {
+
+    public static final String V1_ROUTE = "/v1";
+    public static final String PACKAGE_ROUTE = "/package";
+
+}
