a6ada6f49583f3077346a3cd5d5eb52ee6df9dee
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-18 10:09:37 -0500

    Implemented UpdatedPackageRequest dto

--- src/main/java/com/packagesserviceapi/commons/dtos/UpdatePackageRequest.java
+++ src/main/java/com/packagesserviceapi/commons/dtos/UpdatePackageRequest.java
@@ -1,4 +1,19 @@
 package com.packagesserviceapi.commons.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdatePackageRequest {
+
+    private String recipientName;
+    private String recipientAddress;
+    private String recipientEmail;
+    private String recipientPhoneNumber;
+
 }
