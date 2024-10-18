013e4c908d3f1b4d90ec1c22d42b038c376a0710
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-18 10:23:00 -0500

    Refactored UpdatedPackageRequest dto

--- src/main/java/com/packagesserviceapi/commons/dtos/UpdatePackageRequest.java
+++ src/main/java/com/packagesserviceapi/commons/dtos/UpdatePackageRequest.java
@@ -1,5 +1,7 @@
 package com.packagesserviceapi.commons.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,9 +13,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UpdatePackageRequest {
 
+    @Schema(description = "Recipient's name (optional). Leave blank if no update is needed.")
     private String recipientName;
+
+    @Schema(description = "Recipient's address (optional). Leave blank if no update is needed.")
     private String recipientAddress;
+
+    @Email(message = "Recipient email should be valid")
+    @Schema(description = "Recipient's email (optional). Leave blank if no update is needed.")
     private String recipientEmail;
+
+    @Schema(description = "Recipient's phone number (optional). Leave blank if no update is needed.")
     private String recipientPhoneNumber;
 
 }
