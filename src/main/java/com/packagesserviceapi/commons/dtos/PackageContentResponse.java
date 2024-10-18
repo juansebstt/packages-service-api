276b733ca4d70a23f4358ef661121748bd2499e8
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-18 09:30:17 -0500

    Implemented PackageContentResponse dto

--- src/main/java/com/packagesserviceapi/commons/dtos/PackageContentResponse.java
+++ src/main/java/com/packagesserviceapi/commons/dtos/PackageContentResponse.java
@@ -1,4 +1,18 @@
 package com.packagesserviceapi.commons.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PackageContentResponse {
+
+    private String recipientAddress;
+    private String packageContent;
+    private String packageWeight;
+
 }
