27893b6c4de3c4685b044cd5b7b7d549959ba2e2
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-18 09:23:13 -0500

    Implemented CreatePackageResponse dto

--- src/main/java/com/packagesserviceapi/commons/dtos/CreatePackageResponse.java
+++ src/main/java/com/packagesserviceapi/commons/dtos/CreatePackageResponse.java
@@ -1,4 +1,16 @@
 package com.packagesserviceapi.commons.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreatePackageResponse {
+
+    private Long trackingNumber;
+
 }
