1c0141b85c114c6c874dbb94a70f2802b68da1d1
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-17 22:15:38 -0500

    Partially implemented CreatePackageRequest

--- src/main/java/com/packagesserviceapi/commons/dtos/CreatePackageRequest.java
+++ src/main/java/com/packagesserviceapi/commons/dtos/CreatePackageRequest.java
@@ -1,6 +1,8 @@
 package com.packagesserviceapi.commons.dtos;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,13 +14,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreatePackageRequest {
 
+    @NotBlank
+    @NotNull
     private String recipientName;
 
+    @NotNull
+    @NotBlank
     @Email
     private String receiverEmail;
 
+    @NotNull
+    @NotBlank
     private String address;
+
+    @NotNull
+    @NotBlank
     private String content;
+
+    @NotNull
+    @NotBlank
     private Integer weight;
 
 }
