c820a7ac3abb9cfa5780cdd091d2eb05d51f9ac8
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-25 15:41:25 -0500

    Refactored constanst

--- src/main/java/com/packagesserviceapi/commons/constants/TopicConstants.java
+++ src/main/java/com/packagesserviceapi/commons/constants/TopicConstants.java
@@ -2,6 +2,6 @@ package com.packagesserviceapi.commons.constants;
 
 public class TopicConstants {
 
-    public static final String PACKAGE_CREATED_TOPIC = "package_created";
+    public static final String PACKAGE_CREATED_TOPIC = "package_created_topic";
 
 }
