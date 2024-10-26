9eaebe4deba9ac729028e63517cc6e441bf96ec0
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-26 10:29:23 -0500

    Partially implemented PackageServiceImpl

--- src/main/java/com/packagesserviceapi/service/impl/PackageServiceImpl.java
+++ src/main/java/com/packagesserviceapi/service/impl/PackageServiceImpl.java
@@ -4,9 +4,21 @@ import com.packagesserviceapi.commons.dtos.CreatePackageRequest;
 import com.packagesserviceapi.commons.dtos.CreatePackageResponse;
 import com.packagesserviceapi.commons.dtos.PackageContentResponse;
 import com.packagesserviceapi.commons.dtos.UpdatePackageRequest;
+import com.packagesserviceapi.repositories.PackageRepository;
 import com.packagesserviceapi.service.PackageService;
+import org.springframework.cloud.stream.function.StreamBridge;
 
 public class PackageServiceImpl implements PackageService {
+
+    private final PackageRepository packageRepository;
+    private final StreamBridge streamBridge;
+
+    public PackageServiceImpl(PackageRepository packageRepository, StreamBridge streamBridge) {
+        this.packageRepository = packageRepository;
+        this.streamBridge = streamBridge;
+    }
+
+
     @Override
     public CreatePackageResponse createPackage(CreatePackageRequest createPackageRequest, Long userId) {
         return null;
